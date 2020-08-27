1494bde62d786738836f28a4d4f53e5d7817e4da
Peters <vpe@iks-gmbh.com>
2020-08-27 12:51:39 +0200

    added a save changes function and button to save changes for the display value of the meetings. Also made display changeble from the table

--- src/app/meeting-list/meeting-list.component.ts
+++ src/app/meeting-list/meeting-list.component.ts
@@ -9,6 +9,7 @@ import { Meeting } from './meeting/meeting.model'
 })
 export class MeetingListComponent implements OnInit {
   showMeetings = false;
+  // get from backend later
   meetings: Meeting[] = [
     new Meeting(
       'Testname',
@@ -29,6 +30,8 @@ export class MeetingListComponent implements OnInit {
       false
     )
   ];
+  initialDisplayValues: boolean[] = [];
+  isDisplayDifferent = false;
 
   constructor() { }
 
@@ -36,10 +39,32 @@ export class MeetingListComponent implements OnInit {
     if (localStorage.getItem('login') === 'true') {
       this.showMeetings = true;
     }
+    for (let i = 0; i < this.meetings.length; i++) {
+      this.initialDisplayValues[i] = this.meetings[i].display;
+    }
   }
 
   onAddMeeting() {
     console.log('Veranstaltung hinzufügen geklickt.')
+  }
+
+  onChangeDisplay(i: number) {
+    this.meetings[i].display = !this.meetings[i].display;
+    this.checkForUpdates();
+  }
+
+  checkForUpdates() {
+    for (let i = 0; i < this.meetings.length; i++) {
+      if (this.meetings[i].display != this.initialDisplayValues[i]) {
+        this.isDisplayDifferent = true;
+        break;
+      }
+      this.isDisplayDifferent = false;
+    }
+  }
+
+  onSaveDisplayChanges() {
+    console.log('Änderungen Übernehmen geklickt.')
   }
 
 }

5785b1cf0b7fedf46190512b6e0f462d99355216
Peters <vpe@iks-gmbh.com>
2020-09-08 13:33:28 +0200

    now the add meetings form is prepopulated with the current date and time and it's also the min value. Also added the funktion to go back to the list of meetings and discard then changes with a confirm field

--- src/app/meeting/meeting-edit/meeting-edit.component.ts
+++ src/app/meeting/meeting-edit/meeting-edit.component.ts
@@ -1,6 +1,10 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 import { Meeting } from '../meeting.model';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { ActivatedRoute, Params, Router } from '@angular/router';
+import { MeetingService } from '../meeting.service';
+import { getLocaleDateTimeFormat, DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-meeting-edit',
@@ -8,11 +12,62 @@ import { Meeting } from '../meeting.model';
   styleUrls: ['./meeting-edit.component.css']
 })
 export class MeetingEditComponent implements OnInit {
-  @Input() meeting: Meeting;
+  meetingForm: FormGroup;
+  id: number = 0;
+  meeting: Meeting;
+  now: string;
 
-  constructor() { }
+  constructor(private router: Router, private route: ActivatedRoute, private meetingService: MeetingService) { }
 
   ngOnInit(): void {
+    this.now = new DatePipe('en-US').transform(Date.now(), 'yyyy-MM-ddTHH:mm');
+    this.route.params.subscribe(
+      (params: Params) => {
+        if (params['id'] != null) {
+          this.id = +params['id'];
+        }
+        console.log('id: ' + this.id);
+        this.init();
+      }
+    );
+  }
+
+  private init() {
+
+    console.log(getLocaleDateTimeFormat);
+
+    this.initForm('', this.now, true);
+
+    if (this.id) {
+      this.meetingService.getMeeting(this.id)
+      .subscribe(tempMeeting => {
+        this.meeting = tempMeeting;
+        console.log("DATETIME:");
+        console.log(tempMeeting.datetime);
+        
+        this.initForm(tempMeeting.name, tempMeeting.datetime, tempMeeting.display);
+      });
+    }
+  }
+
+  private initForm(name: string, datetime: string, display: boolean) {
+    
+    this.meetingForm = new FormGroup({
+      'name': new FormControl(name, Validators.required),
+      'datetime': new FormControl(datetime, Validators.required),
+      'display': new FormControl(display)
+    })
+  }
+
+  onSubmit() {
+    console.log(this.meetingForm);
+  }
+
+  onBackToList() {
+    if (this.meetingForm.touched) {
+      if (!(confirm('Änderungen verwerfen?'))) return false;
+    }
+    this.router.navigate(['/meeting/list']);
   }
 
 }

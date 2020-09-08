caf4ec7c0617d7ad3697e6bc72f4815b9a5faab7
Peters <vpe@iks-gmbh.com>
2020-09-08 16:59:19 +0200

    changed route for adding and editing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { MeetingListComponent } from './meeting/meeting-list/meeting-list.component';
+import { MeetingEditComponent } from './meeting/meeting-edit/meeting-edit.component';
 import { LoginComponent } from './login/login.component';
-import { HomeComponent } from './home/home.component';
 import { RegisterComponent } from './login/register/register.component';
+import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'meeting/list', component: MeetingListComponent },
+  { path: 'meeting/add', component: MeetingEditComponent },
+  { path: 'meeting/edit/:id', component: MeetingEditComponent },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
   { path: 'logout', redirectTo: '/home', pathMatch: 'full' },

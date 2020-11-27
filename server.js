5cbf81241b689ddca322661696eee23865593122
Peters <vpe@iks-gmbh.com>
2020-11-27 13:23:34 +0100

    did some changes to be able to deploy to heroku

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/Angular10JwtAuth'));
+app.use(express.static(__dirname + '/dist/MeetingUserApp'));
 
 app.get('/*', function(req,res) {
 
-  res.sendFile(path.join(__dirname+'/dist/Angular10JwtAuth/index.html'));
+  res.sendFile(path.join(__dirname+'/dist/MeetingUserApp/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
